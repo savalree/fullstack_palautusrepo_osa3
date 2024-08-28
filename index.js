60bef7a8cd77c7f39a2a5cd13f45e6fd13505d68
Reetta Savaloja <reetta.savaloja@tietoevry.com>
2024-08-28 08:27:16 +0300

    tehtävä 3.6

--- index.js
+++ index.js
@@ -57,6 +57,24 @@ app.delete('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request,response) => {
     const body = request.body
 
+    if(!body.name){
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+
+    if(!body.number){
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+
+    if(persons.find(person => person.name === body.name)){
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
     const person = {
         id: Math.random(0,1000),
         name: body.name,
