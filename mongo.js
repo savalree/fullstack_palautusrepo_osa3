2e8e33cb54bb3823c6f9b3ede6ee5359a2bdec23
Reetta Savaloja <reetta.savaloja@tietoevry.com>
2024-08-29 16:02:47 +0300

    tehtävä 3.12

---
+++ mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://savalre:${password}@fullstackcoursecluster.kqmyx.mongodb.net/?retryWrites=true&w=majority&appName=fullstackCourseCluster`
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4],
+})
+
+if(!person.name || !person.number){
+    console.log('phonebook:')
+    Person.find({}).then(result => {
+        result.forEach(person => {
+          console.log(person)
+        })
+        mongoose.connection.close()
+      })
+} else {
+    person.save().then(result => {
+        console.log(`added ${result.name} number ${result.number} to phonebook`)
+        mongoose.connection.close()
+      })
+}
